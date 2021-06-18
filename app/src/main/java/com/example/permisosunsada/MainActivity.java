14d0c859ab71aaf4059c40f7b3444c99786eea6e
francobdq <fbdq.dev@gmail.com>
2021-06-18 19:47:05 -0300

    update

--- app/src/main/java/com/example/permisosunsada/MainActivity.java
+++ app/src/main/java/com/example/permisosunsada/MainActivity.java
@@ -1,29 +1,163 @@
 package com.example.permisosunsada;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText username = (EditText)findViewById(R.id.editTextTextPersonName);
-    EditText password = (EditText)findViewById(R.id.editTextTextPassword);
+    //public static String url = "http://www.json-generator.com/api/json/get/cphlRqewGG?indent=2";
+
+    ArrayList<HashMap<String, String>> nameList;
+
+    // para el login
+    EditText username;
+    EditText password;
+
+    // para el spinner
+    Spinner edificiosSpinner;
+    String[] edificios;
+
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // obtengo los editText para verificar usuario y contraseña
+        username = (EditText)findViewById(R.id.usuarioText);
+        password = (EditText)findViewById(R.id.passwordText);
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+        // inicializo el spinner con los valores de los edificios
+        nameList = new ArrayList<>();
+        inicializarSpinner();
+    }
+
+    private void inicializarSpinner(){
+        /*getNames a = new getNames();
+        a.onPreExecute();
+        a.doInBackground();*/
+
+        edificiosSpinner = (Spinner)findViewById(R.id.spinner2);
+
+
+        edificioASpinner();
+
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, edificios);
+        //edificiosSpinner.setAdapter(adapter);
+    }
+
+    private boolean comprobarLogin(){
+        return username.getText().toString().equals("admin") && password.getText().toString().equals("admin");
+    }
+
+    private int comprobarSpinner(){
+        String seleccionado = edificiosSpinner.getSelectedItem().toString();
+
+        if(edificios == null){
+            Toast.makeText(getApplicationContext(), "No se pudo establecer internet. Intente de nuevo más tarde.", Toast.LENGTH_SHORT).show();
+            return -1;
+        }
+
+        for(int i = 0; i < edificios.length;i++){
+            if(edificios[i].equals(seleccionado)){
+                return i;
+            }
+
+        }
+
+        return  -1;
+    }
+
+    private void irAEscanerActivity(int idEdificio){
+        Toast.makeText(getApplicationContext(), "contraseña correcta " + comprobarSpinner(), Toast.LENGTH_SHORT).show();
+        Intent myIntent = new Intent(MainActivity.this, escaner.class);
+        myIntent.putExtra("idEdificio", idEdificio); //Optional parameters
+        MainActivity.this.startActivity(myIntent);
+    }
+
+    public void login(View view) {
+        if (comprobarLogin()) {
+            int idEdificio = comprobarSpinner();
+            if(comprobarSpinner() != -1){
+                irAEscanerActivity(idEdificio+1);
+            }
+
+        } else {
+            Toast.makeText(getApplicationContext(), "contraseña equivocada", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 
-    public void login(View view) {
-        if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+    private void edificioASpinner(){
+        String url = ConexionJSON.HOST + ConexionJSON.peticionEdificios;
 
-            //correcct password
-        } else {
-            //wrong password
-        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println("-------------------------------------------------------------------------------------------------------------------");
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("data");
+                            edificios = new String[jsonArray.length()];
+                            for(int i = 0; i < jsonArray.length(); i++){
+                                JSONObject edificiosJsonArray = jsonArray.getJSONObject(i);
+
+                                String nombre = edificiosJsonArray.getString("nombre");
+                                edificios[i] = nombre;
+                                System.out.println("nombre:" + nombre);
+
+                            }
+
+                            ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, edificios);
+                            edificiosSpinner.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+
+        requestQueue.add(jsonObjectRequest);
     }
 }
--- app/src/main/java/com/example/permisosunsada/escaner.java
+++ app/src/main/java/com/example/permisosunsada/escaner.java
@@ -1,20 +1,227 @@
 package com.example.permisosunsada;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class escaner extends AppCompatActivity {
+
+
+    int idEdificio;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escaner);
+        Intent intent = getIntent();
+        idEdificio = intent.getIntExtra("idEdificio",-1); //if it's a string you stored.
+        requestQueue = Volley.newRequestQueue(escaner.this);
+        //Toast.makeText(getApplicationContext(), "idEdificio: " + idEdificio, Toast.LENGTH_SHORT).show();
     }
 
 
     public void escanear(View view){
-        
+        new IntentIntegrator(this).initiateScan();
+    }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        String valorDelQR = result.getContents();
+
+        if (valorDelQR != null) {
+            Toast.makeText(getApplicationContext(), "QR: "+ valorDelQR, Toast.LENGTH_SHORT).show();
+            QRScaneado(valorDelQR);
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Operacion cancelada", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void updateSolicitud(String id_solicitud) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = ConexionJSON.HOST + ConexionJSON.updateSolicitud + id_solicitud  + "/" + idEdificio;
+
+        final JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("presente", 1);
+        } catch (JSONException e) {
+            // handle exception
+        }
+
+        //Toast.makeText(getApplicationContext(), "Intentanding A", Toast.LENGTH_SHORT).show();
+        JsonObjectRequest putRequest = new JsonObjectRequest(Request.Method.PUT, url, jsonObject,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // response
+                        System.out.println("-----------------------------------------------------------UPDATE SOLICITUD--------------------------------------------------------");
+
+                        try {
+
+                            boolean success = response.getBoolean("success");
+                            Toast.makeText(getApplicationContext(), "Intentanding ", Toast.LENGTH_LONG).show();
+                            if(success){
+                                // todo bien
+                                System.out.println("PUEDE ACCEDER");
+                                Toast.makeText(getApplicationContext(), "TODO OK", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                // edificio incorrecto
+                                System.out.println("Edificio Incorrecto ");
+                                Toast.makeText(getApplicationContext(), "EDIFICIO INCORRECTO " + response.getString("data") + " " + response.getString("message"), Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Toast.makeText(getApplicationContext(), "Error response en update solicitud", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        ) {
+
+            @Override
+            public Map<String, String> getHeaders()
+            {
+                Map<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                headers.put("Accept", "application/json");
+                return headers;
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+
+            @Override
+            public byte[] getBody() {
+
+                try {
+                    Log.i("json", jsonObject.toString());
+                    return jsonObject.toString().getBytes("UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+
+        };
+
+        queue.add(putRequest);
+    }
+
+    /*private void updateSolicitud(String id_solicitud){
+        String url = ConexionJSON.HOST + ConexionJSON.updateSolicitud + id_solicitud  + "/" + idEdificio;
+
+        final JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("presente", 1);
+        } catch (JSONException e) {
+            // handle exception
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.PUT, url, jsonObject, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+
+        requestQueue.add(jsonObjectRequest);
+    }*/
+
+    private void QRScaneado(String qr){
+        String url = ConexionJSON.HOST + ConexionJSON.peticionQR + qr;
+        Toast.makeText(getApplicationContext(), "v2", Toast.LENGTH_SHORT).show();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println("-------------------------------------------------------------------------------------------------------------------");
+                        try {
+                            JSONObject data = response.getJSONObject("data");
+                            if(data == null){
+                                System.out.println("QR INEXISTENTE.");
+                                Toast.makeText(getApplicationContext(), "QR inexistente", Toast.LENGTH_SHORT).show();
+                                return; // QR NO EXISTE.
+                            }
+                            int presenteIngresante = data.getInt("presente");
+                            String  id_solicitud = data.getString("id_solicitud");
+
+                            if(presenteIngresante == 1){
+                                // usuario ya ingresado interfaz
+                                System.out.println("Usuario ya ingresado");
+                                Toast.makeText(getApplicationContext(), "Usuario ya ingresado", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                updateSolicitud(id_solicitud);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        Toast.makeText(getApplicationContext(), "Error response en qrscaneado", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        requestQueue.add(jsonObjectRequest);
     }
 }
